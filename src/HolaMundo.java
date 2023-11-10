9a5aeb91a9f234895dd6dbdf367a9d538f670ab3
lubnamz-20 <lmeziane02@educarex.es>
2023-11-10 13:15:10 +0100

    Bienvenida

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,6 +4,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HOLA MUNDO");
+		
+		System.out.println("Bienvenida");
 	}
 
 }
