fffb7a855321e5a1e73dd094b32302d50b6488a6
lubnamz-20 <lmeziane02@educarex.es>
2023-11-10 13:41:59 +0100

    Saludos Persona

---
+++ src/HolaPersona.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class HolaPersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre=JOptionPane.showInputDialog("Dame tu nombre");
+		
+		JOptionPane.showInternalMessageDialog(null, "Hola "+ nombre);
+				
+				
+				
+				
+				
+		
+		
+		
+	}
+
+}
